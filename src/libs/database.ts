7c24a2b0324374d842f2e1b2351c18c6367b9daf
LoTwT <709937065@qq.com>
2022-01-14 21:35:29 +0800

    refactor: re init whole project with vite

--- src/libs/database.ts
+++
@@ -1,4 +0,0 @@
-import mysql from "mysql2/promise";
-import { db } from "~/config/database"
-
-export default mysql.createPool(db);
--- src/models/banner.ts
+++
@@ -1,6 +0,0 @@
-import db from '../libs/database';
-import { BannerData } from "@/models/banner"
-
-export async function getAllBanner(): Promise<BannerData[]> {
-    return (await db.query('SELECT ID,img,href,color FROM banner_table ORDER BY sort ASC'))[0] as BannerData[];
-}
